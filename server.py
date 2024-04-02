2ad28b706e565e329973bcf386a4ea6ef394b8d6
Harshit M <harshitmeh111@gmail.com>
2024-04-02 14:50:45 +0530

    1

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home_page():
-    return "Hello World"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route("/")
+def home_page():
+    return jsonify({"message": "Hello, World!"})
+
+
+@app.route("/data", methods=["POST"])
+def data():
+    print(request.headers)
+    print(request.args)
+    print(request.form)
+
+    return jsonify({"message": "Data received"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
